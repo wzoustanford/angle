2f0f4c82cc1ac4665702e6d5c75750627de586c3
wzoustanford <wzou@alumni.stanford.edu>
2025-08-05 00:57:06 +0000

    quick modification of model/__init__.py

--- RL/model/__init__.py
+++ RL/model/__init__.py
@@ -1,12 +1,12 @@
 from .dqn_network import DQN
-from .data_buffer import ReplayBuffer, FrameStack
+from .data_buffer import ReplayBuffer, PrioritizedReplayBuffer, FrameStack
 from .dqn_agent import DQNAgent
 from .distributed_buffer import DistributedReplayBuffer, DistributedFrameStack
 from .parallel_env_manager import EnvironmentWorker, ParallelEnvironmentManager
 from .distributed_dqn_agent import DistributedDQNAgent
 
 __all__ = [
-    'DQN', 'ReplayBuffer', 'FrameStack', 'DQNAgent',
+    'DQN', 'ReplayBuffer', 'PrioritizedReplayBuffer', 'FrameStack', 'DQNAgent',
     'DistributedReplayBuffer', 'DistributedFrameStack', 
     'EnvironmentWorker', 'ParallelEnvironmentManager', 'DistributedDQNAgent'
 ]
